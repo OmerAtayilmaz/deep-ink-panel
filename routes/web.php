4d320451c9e0797b975edef142e307bd3271938c
OmerAtayilmaz <omeratayilmaz@gmail.com>
2023-04-07 17:03:39 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,10 @@ use Illuminate\Support\Facades\Route;
         Route::get('/faq', 'faq')->name('faq');
  });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->prefix("/settings")->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('backoffice.settings.edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('backoffice.settings.update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('backoffice.settings.destroy');
 });
 
 require __DIR__.'/auth.php';
